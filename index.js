f2a4bc28299fd22ecdaae23597e3c75c29aa261b
sureshvarma69 <sureshvarma.n@hng.co.in>
2023-07-06 16:23:00 +0530

    carousels added

---
+++ index.js
@@ -0,0 +1,71 @@
+/// carousel 1
+let slideIndex = 1;
+let slideIndex2 = 2;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+}
+
+showSlides(slideIndex);
+
+/// carousel 2
+
+
+
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlidesTwo");
+  let dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+//   dots2[slideIndex2-1].className += " active";
+}
+
+showSlides2(slideIndex2);
+
+
+function loadcarousel(){
+    showSlides(1);
+    showSlides2(1);
+   
+}
