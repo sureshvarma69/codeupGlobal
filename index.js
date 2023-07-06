b7dcadb2c73d330516ae4b2e439f799f2052da5f
sureshvarma69 <sureshvarma.n@hng.co.in>
2023-07-06 17:46:02 +0530

    new carousels added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /// carousel 1
 let slideIndex = 1;
-let slideIndex2 = 2;
+let slideIndex2 = 1;
+let slideIndex3 = 1;
 
 // Next/previous controls
 function plusSlides(n) {
@@ -61,11 +62,38 @@ function showSlides2(n) {
 //   dots2[slideIndex2-1].className += " active";
 }
 
-showSlides2(slideIndex2);
 
+function plusSlides3(n) {
+    showSlides3(slideIndex3 += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide3(n) {
+    showSlides3(slideIndex3 = n);
+  }
+  
+  function showSlides3(n) {
+    let i;
+    let slides3 = document.getElementsByClassName("mySlidesThree");
+    // let dots2 = document.getElementsByClassName("dot");
+    if (n > slides3.length) {slideIndex3 = 1}
+    if (n < 1) {slideIndex3 = slides3.length}
+    for (i = 0; i < slides3.length; i++) {
+      slides3[i].style.display = "none";
+    }
+    // for (i = 0; i < dots2.length; i++) {
+    //   dots2[i].className = dots2[i].className.replace(" active", "");
+    // }
+    slides3[slideIndex3-1].style.display = "block";
+  //   dots2[slideIndex2-1].className += " active";
+  }
+  
+
+showSlides2(slideIndex2);
+// showSlides3(slideIndex3);
 
 function loadcarousel(){
     showSlides(1);
     showSlides2(1);
-   
+    showSlides3(1);
 }
